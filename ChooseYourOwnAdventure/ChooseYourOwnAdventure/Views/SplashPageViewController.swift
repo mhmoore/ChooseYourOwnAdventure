4b65d2c950f0b7680c9f4a3b508c4b419aaa68db
Mike Moore <michaelhmoore1@gmail.com>
2019-09-05 10:05:29 -0600

    finishes UI

--- ChooseYourOwnAdventure/ChooseYourOwnAdventure/Views/SplashPageViewController.swift
+++ ChooseYourOwnAdventure/ChooseYourOwnAdventure/Views/SplashPageViewController.swift
@@ -21,7 +21,14 @@ class SplashPageViewController: UIViewController {
         playButton.layer.borderColor = UIColor.lightGray.cgColor
         playButton.layer.borderWidth = 5.0
         playButton.layer.shadowRadius = 32.0
-        playButton.layer.shadowColor = UIColor.cyan.cgColor
+        playButton.layer.shadowColor = UIColor.black.cgColor
         playButton.layer.shadowOpacity = 1.0
+        
+        UIView.animate(withDuration: 3.0, delay: 0, options: [.transitionFlipFromTop, .allowUserInteraction, .autoreverse, .repeat], animations: {
+            self.view.layer.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)
+        }, completion: nil)
+        UIView.animate(withDuration: 3.0, delay: 3.0, options: [.transitionFlipFromBottom, .allowUserInteraction, .autoreverse, .repeat], animations: {
+            self.view.layer.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        }, completion: nil)
     }
 }
